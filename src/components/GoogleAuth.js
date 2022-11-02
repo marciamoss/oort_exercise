95e7f641c87297e93c674331ee695640771cfcbd
Marcia Moss <marcia_moss@hotmail.com>
2022-11-01 20:53:53 -0400

    updated oauth client id

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -8,7 +8,7 @@ class GoogleAuth extends React.Component {
       window.gapi.client
         .init({
           clientId:
-            '797401886567-9cumct9mrt3v2va409rasa7fa6fq02hh.apps.googleusercontent.com',
+            '650821786438-kd45tu35ib0ohqoeosi41vl483njntev.apps.googleusercontent.com',
           scope: 'email',
           plugin_name:'oort'
         })
